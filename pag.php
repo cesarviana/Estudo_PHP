2644ad0ac9740bfa19698ccd6f551b40d80b7767
cesarviana <cesar.pviana@gmail.com>
2013-12-25 11:54:33 -0200

    Concatenação

--- pag.php
+++ pag.php
@@ -8,6 +8,8 @@
 		// Exemplo 4_3 livro Novatec
 		define("meunome","Cesar");
 		define("peso","69");
+		
+		// a concatenação é feita com pontos
 		echo "Meu nome é " . meunome . ".<br/>";
 		echo "Peso = " . peso . " quilos.<br/>";
 	?>
