1d1cf638baf36d887404a29e61870d68bac34f72
cesarviana <cesar.pviana@gmail.com>
2013-12-27 15:26:45 -0200

    Tipos de variáveis - numéricas

--- pag.php
+++ pag.php
@@ -5,12 +5,17 @@
 </head>
 <body>
 	<?php 
-		// Variáveis criadas durante a execução
-		$texto = "time";
-		$$texto;
-		$time = "Internacional";
+		// Tipos de variáveis - numéricas
+		$inteiro = 10;
+		$decimal = 10.01;
+		$hexadecimal = 0xF;
+		$octal = 010;
+		echo $inteiro . "<br/>";
+		echo $decimal . "<br/>";
+		echo (int) $hexadecimal . "<br/>";
+		echo (int) $octal . "<br/>";
 		
-		echo "Meu time é o $time.";
+		
 	?>
 </body>
 </html>
