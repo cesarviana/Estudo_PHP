2938785b4bb0ee1d28a9b1330392ec2c05b75869
cesarviana <cesar.pviana@gmail.com>
2013-12-30 12:01:56 -0200

    Operadores - unários

--- pag.php
+++ pag.php
@@ -6,12 +6,16 @@
 <body>
 	<?php 
 	// Operadores 
-		// aritiméticos
-			$a = 1 + 1;
-			$a = 1 - 1;
-			$a = 1 * 10;
-			echo $a / 5;
-			echo $a % 5;	
+		// unários
+			$contador = 0;
+			echo $contador++; // mostra 0 e incrementa 0 -> 1
+			echo ++$contador; // incrementa 1 -> 2 (depois mostra)
+			echo --$contador; // decrementa 2 - 1 (depois mostra)
+			echo $contador--; // mostra 1 e decrementa 1 -> 0
+			
+			// saida = 02110
+			
+			echo -$contador;
 	?>
 </body>
 </html>
