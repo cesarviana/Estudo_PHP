d4597fa0bdb670c262a6707f5ad10630a03461ea
cesarviana <cesar.pviana@gmail.com>
2013-12-31 19:31:51 -0200

    Estruturas de controle - Condicionais 2

--- pag.php
+++ pag.php
@@ -13,6 +13,20 @@
 		else:
 			echo "Reprovado";
 		endif;
+		
+		// switch
+		$opcao = "SIM";
+		switch ($opcao)
+		{
+			case "SIM" :
+				echo "<br> Você aceitou os termos.";
+				break;
+			case "NAO" :
+				echo "<br> Você rejetiou os termos.";
+				break;
+			default:
+				echo "<br> A opção escolhida é inválida.";
+		}
 	?>
 </body>
 </html>
